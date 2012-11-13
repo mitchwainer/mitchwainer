fc57e87f06154c58692acfab56b98d3f233809c0
Mitch <mitch@digitalocean.com>
2012-11-13 16:26:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mitch::Application.config.secret_token = 'c643690118f6ae6dbec934e84031973d634b67e0c4e738999c0c5ca23826e0cea7d887b9de498c22b9b69afadc475fcfbd3d21095c09a9a422fd841ea0cf974e'
